5180791810178c85af8350efbeddcf82308eec7e
dineshramv13 <dineshramv13@gmail.com>
2024-10-27 21:49:21 +0530

    Deployment created

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -93,8 +93,8 @@ class DataIngestion:
             dataframe=self.export_collection_as_dataframe()
             dataframe=self.export_data_into_feature_store(dataframe)
             self.split_data_as_train_test(dataframe)
-            dataingestionartifact=DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
-                                                        test_file_path=self.data_ingestion_config.testing_file_path)
+            dataingestionartifact=DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,test_file_path=self.data_ingestion_config.testing_file_path)
+            
             return dataingestionartifact
 
         except Exception as e:
